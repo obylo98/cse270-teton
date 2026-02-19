1041befbed9395e46caa0027700c56db9945a706
obylo98 <obinnaandrewonyekwere@gmail.com>
2026-02-19 12:11:26 -0600

    week 6

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,12 +1,6 @@
 import pytest
 import requests
 
-@pytest.fixture
-def mock_requests_get(mocker):
-    response_json = {"businesses":[{"name":"Teton Elementary"}]}
-    
-    # Patching requests.get to return a mocked response
-    mocker.patch.object(requests, 'get', return_value=MockResponse(response_json))
 
 class MockResponse:
     def __init__(self, json_data, status_code=200):
@@ -16,9 +10,16 @@ class MockResponse:
     def json(self):
         return self.json_data
 
+
+@pytest.fixture
+def mock_requests_get(mocker):
+    response_json = {"businesses": [{"name": "Teton Elementary"}]}
+    mocker.patch.object(requests, "get", return_value=MockResponse(response_json))
+
+
 def test_data_endpoint(mock_requests_get):
     # Make the HTTP GET request
-    response = requests.get('http://127.0.0.1:8000/data/all')
+    response = requests.get("http://127.0.0.1:8000/data/all")
 
     # Verify the response status code
     assert response.status_code == 200
@@ -26,12 +27,12 @@ def test_data_endpoint(mock_requests_get):
     # Verify the response content
     data = response.json()
     assert isinstance(data, dict)
-    assert 'businesses' in data
-    businesses = data['businesses']
+    assert "businesses" in data
+    businesses = data["businesses"]
     assert isinstance(businesses, list)
     assert len(businesses) > 0
     first_business = businesses[0]
     assert isinstance(first_business, dict)
-    assert 'name' in first_business
-    assert first_business['name'] == 'Teton Elementary'
+    assert "name" in first_business
+    assert first_business["name"] == "Teton Elementary"
     # Add additional assertions as needed based on the expected response
